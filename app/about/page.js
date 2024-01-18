d308714c4a32a887896f5e7b6b27ea6bbee93b4e
Nasiful Islam Nasif <nasifulislamnasif23@gmail.com>
2024-01-19 00:12:54 +0600

    practice router in next js

---
+++ app/about/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function about () {
+  return (
+    <div className='mt-10'>This is about page</div>
+  )
+}
+
+export default about
--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,8 +11,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html className='p-6' lang="en">
+      <body className={inter.className}>
+        <nav><Link href={"/"}>Home</Link> | <Link href={"/about"}>About</Link></nav>
+        {children}
+      </body>
     </html>
   )
 }
